fc2100740bcebf1dee686b29f7dac3051fad1e17
Simon <simon.wolflobo@gmail.com>
2018-10-03 10:45:15 -0400

    Finished ts-file, used OOP

--- reader.js
+++ reader.js
@@ -1,27 +1,32 @@
 var fs = require('fs');
-//var list = document.getElementById('words');
 var sorted = [];
-fs.readFile('hitch.txt', 'utf8', function (err, data) {
-    if (err)
-        throw err;
-    var reg = /\n| /;
-    var clean = data.split(reg);
-    var count = {};
-    for (var _i = 0, clean_1 = clean; _i < clean_1.length; _i++) {
-        var i = clean_1[_i];
-        count[i] = (count[i] || 0) + 1;
+var counter = /** @class */ (function () {
+    function counter(name) {
+        this.filename = name;
     }
-    for (var key in count)
-        sorted.push([key, count[key]]);
-    sorted.sort(function (a, b) {
-        return a[1] - b[1];
-    });
-    sorted.reverse();
-    for (var i = 1; i <= 9; i++) {
-        console.log(i + ". '" + sorted[i][0] + "' . Antal uppkomster: " + sorted[i][1]);
-    }
-});
-/* var liNode = document.createElement("li");
-var newitem = document.createTextNode(sorted[i][0]+ " antal uppkomster: "+  sorted[i][1]);
-liNode.appendChild(newitem);
-list.appendChild(liNode); */ 
+    counter.prototype.showFile = function () {
+        fs.readFile(this.filename, 'utf8', function (err, data) {
+            if (err)
+                throw err;
+            var reg = /\W+/g;
+            var clean = data.split(reg);
+            var count = {};
+            for (var _i = 0, clean_1 = clean; _i < clean_1.length; _i++) {
+                var i = clean_1[_i];
+                count[i] = (count[i] || 0) + 1;
+            }
+            for (var key in count)
+                sorted.push([key, count[key]]);
+            sorted.sort(function (a, b) {
+                return a[1] - b[1];
+            });
+            sorted.reverse();
+            for (var i = 0; i <= 9; i++) {
+                console.log((i + 1) + ". '" + sorted[i][0] + "' . Antal uppkomster: " + sorted[i][1]);
+            }
+        });
+    };
+    return counter;
+}());
+var wordCounter = new counter("hitch.txt");
+wordCounter.showFile();
--- reader.ts
+++ reader.ts
@@ -1,32 +1,40 @@
 declare function require (name:string);
 var fs = require('fs');
-//var list = document.getElementById('words');
 var sorted = [];
 
+class counter {
+    filename: string;
 
-fs.readFile('hitch.txt', 'utf8', function(err, data ){
-    if(err) throw err;
-    let reg: RegExp = /\n| /;
-    let clean = data.split(reg);
-    let count = {};
-    for(let i of clean){
-        count[i] = (count[i]||0) + 1;
+    constructor(name: string) {
+        this.filename = name;
+    } 
+
+    showFile(): void {
+        fs.readFile(this.filename, 'utf8', function(err, data ){
+            if(err) throw err;
+            let reg: RegExp = /\W+/g;
+            let clean = data.split(reg);
+            let count = {};
+            for(let i of clean){
+                count[i] = (count[i]||0) + 1;
+            }
+            
+            for(let key in count) sorted.push ([key, count[key]]);
+            sorted.sort(function (a, b){
+                return a[1] - b[1]
+            });
+            sorted.reverse();
+            for(let i = 0; i <= 9; i++){
+                console.log((i+1) + ". '"+sorted[i][0]+ "' . Antal uppkomster: "+  sorted[i][1]);
+            }
+        
+        });  
     }
-    
-    for(let key in count) sorted.push ([key, count[key]]);
-    sorted.sort(function (a, b){
-        return a[1] - b[1]
-    });
-    sorted.reverse();
-    for(let i = 1; i <= 9; i++){
-        console.log(i + ". '"+sorted[i][0]+ "' . Antal uppkomster: "+  sorted[i][1]);
-    }
+}
 
-});
+let wordCounter = new counter("hitch.txt");
+wordCounter.showFile();
 
 
 
-/* var liNode = document.createElement("li");
-var newitem = document.createTextNode(sorted[i][0]+ " antal uppkomster: "+  sorted[i][1]);
-liNode.appendChild(newitem);
-list.appendChild(liNode); */
+
